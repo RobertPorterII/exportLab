4212580d101f8b76fee9345c1bf5ede9a8aff302
RobertPorterII <au8494@protonmail.com>
2024-08-29 11:00:30 -0400

    First commit

---
+++ Stack.js
@@ -0,0 +1,29 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    // Add a new element to the stack
+    push(element) {
+        // Your code here
+    }
+    // Remove the top element from the stack
+    pop() {
+        // Your code here
+    }
+    // View the top element in the stack
+    peek() {
+        // Your code here
+    }
+    // Check if the stack is empty
+    isEmpty() {
+        // Your code here
+    }
+    // Get the size of the stack
+    size() {
+        // Your code here
+    }
+    // Clear the stack
+    clear() {
+        // Your code here
+    }
+}
