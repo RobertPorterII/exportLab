09e29b3c1101a8f4825e3f46ce08c47378537cf6
RobertPorterII <au8494@protonmail.com>
2024-08-29 12:08:05 -0400

    Final Commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-
-
+import { Stack } from "./Stack.js"
 const stack = new Stack()
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack.peek());
+console.log(stack.isEmpty());
+console.log(stack.size());
+stack.clear();
+console.log(stack.isEmpty());
+
+
+
+
+
+
